f266a7e71c02dbd2be975470b20d841fb156873c
yuang.zya <yuang.zya@alibaba-inc.com>
2018-09-28 16:43:03 +0800

    feat: 接入redis做缓存

--- src/index.js
+++ src/index.js
@@ -5,96 +5,118 @@ const request = require('superagent')
 const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
-const { mkdirs, getProcessCwd } = require('./util')
-const nunjucks = require('nunjucks')
+const { mkdirs, getProcessCwd, renderJsTpl, enHance, insertBigviewRuntime } = require('./util')
+const redis = require('redis')
+const client = redis.createClient()
+const { promisify } = require('util')
+const getAsync = promisify(client.get).bind(client)
 
 async function html2Bigvew (ctx, url) {
-  ctx.render = function (tpl, data, cb) {
-    const env = nunjucks.configure('/')
-    if (/\.nj$/.test(tpl) || /\.html$/.test(tpl)) {
-      env.render(tpl, data, (err, html) => {
-        cb && cb(err, html)
+  enHance(ctx) // 给ctx增加渲染模版功能
+  const bigView = new BigView(ctx)
+  const mainFunc = async (html) => {
+    let layout
+    let $ = cheerio.load(html, {decodeEntities: false}) // 关闭html实体编码转换
+    if ($('#bp-main').length !== 0) {
+      let mainPageLet = await getAsync(`${url}mainPageLet`)
+      if (mainPageLet) {
+        bigView.main = mainPageLet
+      } else {
+        // 爬取bp-main
+        let bpMain = $('#bp-main').html()
+        mkdirs(path.join(getProcessCwd('./src/tpl/bp-main')))
+        fs.writeFileSync(getProcessCwd('./src/tpl/bp-main/index.nj'), bpMain)
+        await renderJsTpl(ctx, '../jstpl/mainjstpl.nj', 'bp-main')
+        mainPageLet = require(getProcessCwd('./src/tpl/bp-main/index.js'))
+        client.set(`${url}mainPageLet`, mainPageLet)
+        bigView.main = mainPageLet
+      }
+      $('#bp-main').empty()
+    }
+    let allBlglet = $('.biglet')
+    let bigletArr = await getAsync(`${url}biglet`)
+    if (bigletArr !== null && bigletArr.length !== 0) {
+      bigletArr.map(item => {
+        bigView.add(item)
       })
     } else {
-      env.renderString(tpl, data, (err, html) => {
-        cb(err, html)
-      })
+      for (let i = 1; i <= allBlglet.length; i++) {
+        // 爬取biglet
+        let biglet = $(`#biglet_${i}`).html()
+        mkdirs(getProcessCwd(`./src/tpl/biglet_${i}`))
+        fs.writeFileSync(getProcessCwd(`./src/tpl/biglet_${i}/index.nj`), biglet)
+        await renderJsTpl(ctx, '../jstpl/bigletchildren.nj', `biglet_${i}`)
+        const bigletObj = require(getProcessCwd(`./src/tpl/biglet_${i}`))
+        bigletArr.push(bigletObj)
+        bigView.add(bigletObj)
+        $(`#biglet_${i}`).empty()
+      }
+      client.set(`${url}biglet`, bigletArr)
     }
+
+    let bplayout = await getAsync(`${url}bplayout`)
+    if (bplayout) {
+      bigView.layout = bplayout
+    } else {
+      // 爬取layout
+      layout = $.html('html')
+      // 插入bigview.runtime.js
+      layout = insertBigviewRuntime(layout)
+      mkdirs(getProcessCwd('./src/tpl/bp-layout'))
+      fs.writeFileSync(getProcessCwd(`./src/tpl/bp-layout/index.nj`), layout)
+      await renderJsTpl(ctx, '../jstpl/layoutjstpl.nj', 'bp-layout')
+      // set layout
+      const bplayout = require(getProcessCwd('./src/tpl/bp-layout'))
+      bigView.layout = bplayout
+      client.set(`${url}bplayout`, bplayout)
+    }
+    // you can custom bigView dataStore
+    bigView.dataStore = {}
+    // bigView.mode = 'pipeline'
+    ctx.status = 200
+    await bigView.start()
   }
-
-  ctx.complie = function (str) {
-    const template = nunjucks.compile(str)
-    return template
-  }
-
-  const bigView = new BigView(ctx)
-
   if (url) {
     return new Promise(resolve => {
-      request
-        .get(url)
-        .set('Content-Type', 'text/html; charset=UTF-8')
-        .end(async (err, rep) => {
-          if (err) {
-            console.log(err)
-            return
-          }
-          let html = rep.text
-          let layout
-          let $ = cheerio.load(html, {decodeEntities: false}) // 关闭html实体编码转换
-          const renderJsTpl = (route, name) => {
-            return new Promise(resolve => {
-              ctx.render(path.join(__dirname, route), {
-                name: name
-              }, (err, html) => {
-                if (err) {
-                  console.log(err)
-                  return
-                }
-                fs.writeFileSync(getProcessCwd(`./src/tpl/${name}/index.js`), html)
-                resolve()
-              })
-            })
-          }
-
-          if ($('#bp-main').length !== 0) {
-            // 爬取bp-main
-            let bpMain = $('#bp-main').html()
-            mkdirs(path.join(getProcessCwd('./src/tpl/bp-main')))
-            fs.writeFileSync(getProcessCwd('./src/tpl/bp-main/index.nj'), bpMain)
-            await renderJsTpl('./jstpl/mainjstpl.nj', 'bp-main')
-            const mainPageLet = require(getProcessCwd('./src/tpl/bp-main/index.js'))
-            bigView.main = mainPageLet
-            $('#bp-main').empty()
-          }
-
-          let allBlglet = $('.biglet')
-          for (let i = 1; i <= allBlglet.length; i++) {
-            // 爬取biglet
-            let biglet = $(`#biglet_${i}`).html()
-            mkdirs(getProcessCwd(`./src/tpl/biglet_${i}`))
-            fs.writeFileSync(getProcessCwd(`./src/tpl/biglet_${i}/index.nj`), biglet)
-            await renderJsTpl('./jstpl/bigletchildren.nj', `biglet_${i}`)
-            bigView.add(require(getProcessCwd(`./src/tpl/biglet_${i}`)))
-            $(`#biglet_${i}`).empty()
-          }
-          // 爬取layout
-          layout = $.html('html')
-          mkdirs(getProcessCwd('./src/tpl/bp-layout'))
-          fs.writeFileSync(getProcessCwd(`./src/tpl/bp-layout/index.nj`), layout)
-
-          await renderJsTpl('./jstpl/layoutjstpl.nj', 'bp-layout')
-
-          // set layout
-          bigView.layout = require(getProcessCwd('./src/tpl/bp-layout'))
-
-          // you can custom bigView dataStore
-          bigView.dataStore = {}
-
-          // bigView.mode = 'pipeline'
-          await bigView.start()
+      client.get(url, async (err, rep) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        if (rep !== null) {
+          // 如果redis有缓存结果就直接从redis获取
+          await mainFunc(rep)
           resolve()
-        })
+          request
+            .get(url)
+            .set('Content-Type', 'text/html; charset=UTF-8')
+            .end((err, rep) => {
+              if (err) {
+                console.log(err)
+                return
+              }
+              let html = rep.text
+              if (html !== rep) {
+                client.flushall() // 源码改变就清空所有缓存
+                client.set(url, html) // 缓存新的html
+              }
+            })
+        } else {
+          request
+            .get(url)
+            .set('Content-Type', 'text/html; charset=UTF-8')
+            .end(async (err, rep) => {
+              if (err) {
+                console.log(err)
+                return
+              }
+              let html = rep.text
+              client.set(url, html) // 缓存爬虫结果
+              await mainFunc(html)
+              resolve()
+            })
+        }
+      })
     })
   }
 }
--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const nunjucks = require('nunjucks')
 
 const mkdirs = (route) => {
   if (fs.existsSync(route)) {
@@ -18,7 +19,53 @@ const mkdirs = (route) => {
 const getProcessCwd = (route) => {
   return path.join(process.cwd(), route)
 }
+
+const renderJsTpl = (ctx, route, name) => {
+  return new Promise(resolve => {
+    ctx.render(path.join(__dirname, route), {
+      name: name
+    }, (err, html) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      fs.writeFileSync(getProcessCwd(`./src/tpl/${name}/index.js`), html)
+      resolve()
+    })
+  })
+}
+
+const enHance = (ctx) => {
+  ctx.render = function (tpl, data, cb) {
+    const env = nunjucks.configure('/')
+    if (/\.nj$/.test(tpl) || /\.html$/.test(tpl)) {
+      env.render(tpl, data, (err, html) => {
+        cb && cb(err, html)
+      })
+    } else {
+      env.renderString(tpl, data, (err, html) => {
+        cb(err, html)
+      })
+    }
+  }
+
+  ctx.complie = function (str) {
+    const template = nunjucks.compile(str)
+    return template
+  }
+}
+
+const insertBigviewRuntime = (layout) => {
+  const bodyIndex = layout.indexOf('</body>')
+  layout = layout.substring(0, bodyIndex)
+  layout += '<script src="//g.alicdn.com/ku/bigview.runtime/1.4.5/bigview.runtime.min.js"></script></body></html>'
+  return layout
+}
+
 module.exports = {
   mkdirs,
-  getProcessCwd
+  getProcessCwd,
+  renderJsTpl,
+  enHance,
+  insertBigviewRuntime
 }
